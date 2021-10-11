8d47ba3847b79674cf9f869dbf206a827b987c18
BastianVIA <70633697+BastianVIA@users.noreply.github.com>
2021-10-12 01:46:20 +0200

    First and last commit

---
+++ A1/Data/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Models;
+
+namespace A1.Data
+{
+    public interface IUserService
+    {
+        User ValidateUser(string userName, string password);
+    }
+}
---
+++ A1/Data/Impl/InMemoryUserService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Models;
+
+namespace A1.Data.Impl
+{
+    public class InMemoryUserService : IUserService {
+        private List<User> users;
+
+        public InMemoryUserService() {
+            users = new[] {
+                new User {
+                    Password = "admin",
+                    SecurityLevel = 5,
+                    UserName = "admin"
+                },
+                new User {
+                    Password = "123456",
+                    SecurityLevel = 3,
+                    UserName = "Bastian"
+                },
+                new User {
+                    Password = "123456",
+                    SecurityLevel = 1,
+                    UserName = "Mikkel"
+                }
+            }.ToList();
+        }
+
+
+        public User ValidateUser(string userName, string password) {
+            User first = users.FirstOrDefault(user => user.UserName.Equals(userName));
+            if (first == null) {
+                throw new Exception("User not found");
+            }
+
+            if (!first.Password.Equals(password)) {
+                throw new Exception("Incorrect password");
+            }
+
+            return first;
+        }
+    }
+}
